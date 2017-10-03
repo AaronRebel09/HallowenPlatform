8e88e0dd32a643914b12c6e0241f04ca571668b1
Rodrigo Aaron <flamasrebel01@gmail.com>
2017-10-02 19:59:14 -0500

    first version

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float moveSpeed;
+	private bool facingRight = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			float y = GetComponent<Rigidbody2D> ().velocity.y;
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed,y);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			float y = GetComponent<Rigidbody2D> ().velocity.y;
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed,y);
+		}
+
+		if (facingRight) {
+			Flip ();
+		}
+
+		if (!facingRight) {
+			Flip ();
+		}
+
+	}
+
+	void Flip(){
+		Vector3 flipScale;
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D> ();
+		flipScale = rigidBody.transform.localScale;
+		flipScale.x *= -1; // flip horizontally, along x-axis
+		rigidBody.transform.localScale = flipScale;
+		facingRight = !facingRight; //facing opposite direction.
+	}
+		
+
+}
